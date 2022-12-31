342accb3fea9f023e4fee89f821708388f90edf5
alow-akter <kironalow453@gmail.com>
2022-12-31 18:56:37 -0500

    our all service

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,10 +1,29 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './MakeAppointments/AppointmentOption';
 
 const AvailableAppointments = ({ seletedDate }) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([])
+
+    useEffect(() => {
+        fetch('appointmentOption.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }
+        , [])
     return (
-        <section className='mt-6'>
+        <section className='my-6'>
             <p className='text-center text-2xl font-semibold text-pink-600'>Available Appointments on {format(seletedDate, 'PP')}  </p>
+            <hr className='w-1/2 mx-auto mt-4' />
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOptions={option}
+                    >
+                    </AppointmentOption>)
+                }
+            </div>
         </section>
     );
 };
---
+++ src/Pages/Appointment/MakeAppointments/AppointmentOption.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AppointmentOption = ({ appointmentOptions }) => {
+    const { name, slots } = appointmentOptions
+    return (
+        <div className="card  shadow-xl mt-10 lg:mx-6">
+            <div className="card-body justify-center items-center">
+                <h2 className="card-title text-center text-pink-600">{name}</h2>
+                <p>{slots.length > 0 ? slots[0] : 'Try another day'}</p>
+                <p>{slots.length} space available</p>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-primary bg-pink-400 border-none text-white">Book Appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AppointmentOption;
